58a4c4595325c8793751fdede34fa9333ab94bf3
GitHub Action <action@github.com>
2020-01-07 16:47:37 +0000

    rodbus doc update: vtest

---
+++ rodbus/vtest/search/classes_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['result',['Result',['../struct_result.html',1,'']]],
+  ['runtimeconfig',['RuntimeConfig',['../struct_runtime_config.html',1,'']]]
+];
---
+++ rodbus/vtest/search/variables_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['runtime',['runtime',['../struct_session.html#abfbd41ae96552b324c8f6ba6287ad68b',1,'Session']]]
+];

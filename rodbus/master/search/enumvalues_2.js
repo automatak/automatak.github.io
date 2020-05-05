b461be7c43b33ae9bc62e4758f7ff2d2357a6a0b
GitHub Action <action@github.com>
2020-05-05 17:22:30 +0000

    rodbus doc update: master

--- rodbus/master/search/enumvalues_2.js
+++ rodbus/master/search/enumvalues_2.js
@@ -5,7 +5,7 @@ var searchData=
   ['status_5fbad_5fresponse',['STATUS_BAD_RESPONSE',['../rodbus_8h.html#a67a0db04d321a74b7e7fcfd3f1a3f70ba4b03b4e872682fcc47146c62e0f4d4ee',1,'rodbus.h']]],
   ['status_5fexception',['STATUS_EXCEPTION',['../rodbus_8h.html#a67a0db04d321a74b7e7fcfd3f1a3f70ba376a5fd798c7c418fdf4a664e648f984',1,'rodbus.h']]],
   ['status_5finternal_5ferror',['STATUS_INTERNAL_ERROR',['../rodbus_8h.html#a67a0db04d321a74b7e7fcfd3f1a3f70badbee399e52770145a726e8bf2a7d6c83',1,'rodbus.h']]],
-  ['status_5fioerror',['STATUS_IOERROR',['../rodbus_8h.html#a67a0db04d321a74b7e7fcfd3f1a3f70ba89023aa09e29f81a7fc26f02c78d798a',1,'rodbus.h']]],
+  ['status_5fio_5ferror',['STATUS_IO_ERROR',['../rodbus_8h.html#a67a0db04d321a74b7e7fcfd3f1a3f70ba6373919fa18245967d76144389ca22fd',1,'rodbus.h']]],
   ['status_5fno_5fconnection',['STATUS_NO_CONNECTION',['../rodbus_8h.html#a67a0db04d321a74b7e7fcfd3f1a3f70bab08ccce70f99d316ca27d1b008d42226',1,'rodbus.h']]],
   ['status_5fok',['STATUS_OK',['../rodbus_8h.html#a67a0db04d321a74b7e7fcfd3f1a3f70ba7e4a42e3b6dd63708c64cf3db6f69566',1,'rodbus.h']]],
   ['status_5fresponse_5ftimeout',['STATUS_RESPONSE_TIMEOUT',['../rodbus_8h.html#a67a0db04d321a74b7e7fcfd3f1a3f70badf018f95e71f6974db3f5c46ca93cb23',1,'rodbus.h']]],
